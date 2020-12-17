94c737cfad4a2fc08af826d63f25429dd0450ab4
Rodrigo Beker <rodrigobeker18@gmail.com>
2020-12-17 13:40:42 -0300

    Initial commit - Spring initialzr

---
+++ src/main/java/br/com/secretariadeeducacao/apiescolasecretariadeeducacao/ApiEscolaSecretariadeeducacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.secretariadeeducacao.apiescolasecretariadeeducacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEscolaSecretariadeeducacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEscolaSecretariadeeducacaoApplication.class, args);
+	}
+
+}
